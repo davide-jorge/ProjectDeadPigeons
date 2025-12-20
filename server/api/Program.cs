b18dfaf060d920f474c48fb4ee717be38528ada1
davide-jorge <finndahumane3@protonmail.com>
2025-12-20 19:10:36 +0100

    running api

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,31 @@
+using dataccess;
+using dataccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetValue<string>("Db"));
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+{
+    var myUser = new User()
+    {
+        Name = "James",
+        PasswordHash = "password",
+        Role = "Admin",
+        CreatedAt = DateTime.Now,
+        Id =  Guid.NewGuid()
+    };
+    dbContext.Users.Add(myUser);
+    dbContext.SaveChanges();
+    var objects = dbContext.Users.ToList();
+    return objects;
+});
 
 app.Run();
