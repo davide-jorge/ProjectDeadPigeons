dce4fdf9980179881993a9f5680ca7dd607da679
davide-jorge <finndahumane3@protonmail.com>
2025-12-29 17:19:21 +0100

    deployment

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,18 +1,28 @@
+using System.Text.Json;
+using api;
 using dataccess;
 using dataccess.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+Console.WriteLine(JsonSerializer.Serialize(appOptions));
+
 builder.Services.AddDbContext<MyDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetValue<string>("Db"));
+    options.UseNpgsql(appOptions.DbConnectionString);
 });
 
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
+app.MapGet("/", (
+    
+    [FromServices] IOptionsMonitor<AppOptions> optionsMonitor,
+    [FromServices] MyDbContext dbContext) =>
 {
     var myUser = new User()
     {
