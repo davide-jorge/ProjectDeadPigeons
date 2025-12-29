cf6694def2d4031a961e5df96242fa7182d88015
davide-jorge <finndahumane3@protonmail.com>
2025-12-29 17:29:52 +0100

    test change deployment

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 var appOptions = builder.Services.AddAppOptions(builder.Configuration);
 
-Console.WriteLine(JsonSerializer.Serialize(appOptions));
+Console.WriteLine("App Options: " + JsonSerializer.Serialize(appOptions));
 
 builder.Services.AddDbContext<MyDbContext>(options =>
 {
